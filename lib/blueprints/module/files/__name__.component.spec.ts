938eadfcb052c524e6e473325630570eb0fa23e9
Valentin GOT <gotvalentin@gmail.com>
2017-03-29 21:14:56 +0200

    feat(blueprint-module): generate spec file based on the configuration file

--- lib/blueprints/module/files/__name__.component.spec.ts
+++ lib/blueprints/module/files/__name__.component.spec.ts
@@ -1,9 +1,20 @@
-import { <%= classifiedName %>Component } from './<%= fileName %>.component';
+import * as angular from 'angular';
+<% if (appModulePath) { %>
+import { <%= appModuleName %> } from '<%= appModulePath %>';<% } %>
+import { <%= classifiedName %>Module } from './<%= fileName %>.module';
 
 describe ('<%= classifiedName %>Component', () => {
+  let <%= camelizedName %>Controller;
+  <% if (appModulePath) { %>
+  beforeEach (angular.mock.module(<%= appModuleName %>));<% } %>
+  beforeEach (angular.mock.module(<%= classifiedName %>Module));
+
+  beforeEach (angular.mock.inject(($componentController: angular.IComponentControllerService) => {
+    <%= camelizedName %>Controller = $componentController('<%= camelizedName %>', {}, {});
+  }));
 
   it ('should create', () => {
-    expect(<%= classifiedName %>Component).toBeTruthy();
+    expect(<%= camelizedName %>Controller).toBeTruthy();
   });
 
 });
