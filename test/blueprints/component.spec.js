abb0d5259fca2a5236fcc0d5a5b2450739011927
Valentin GOT <gotvalentin@gmail.com>
2017-04-12 17:08:27 +0200

    test(component): add a new test for relative path generation

--- test/blueprints/component.spec.js
+++ test/blueprints/component.spec.js
@@ -5,40 +5,60 @@ const fs = require('fs-extra'),
   expect = require('chai').expect,
 
   Generate = require('../../lib/commands/generate'),
-  helper = new (require('../helper'))(),
-  root = process.cwd(),
-  testPath = path.join(root, 'tmp', 'src', 'app'),
+  helper = new (require('../helper'))();
+
+const appDir = `src${path.sep}app`,
   BLUEPRINT = 'component';
 
 describe ('Blueprint::Component', () => {
+  let project;
 
-  beforeEach (() => helper.setup());
+  beforeEach (() => helper.setup().then((pjt) => {
+    project = pjt;
+  }));
 
   it ('should generate component my-test-component', (done) => {
     Generate.create(BLUEPRINT, 'my-test-component', {
       quiet: true
     }).then(() => {
-      expect(fs.existsSync(path.join(testPath, 'my-test-component', 'my-test-component.component.ts'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'my-test-component', 'my-test-component.component.spec.ts'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'my-test-component', 'my-test-component.component.scss'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'my-test-component', 'my-test-component.component.html'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'my-test-component', 'my-test-component.controller.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.spec.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.scss'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.html'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.controller.ts'))).to.equal(true);
 
       done();
     }).catch((err) => done(err));
   });
 
   it ('should generate component sub/my-test-component', (done) => {
-    fs.mkdirSync(path.join(testPath, 'sub'));
+    fs.mkdirSync(path.join(project.root, appDir, 'sub'));
 
     Generate.create(BLUEPRINT, 'sub/my-test-component', {
       quiet: true
     }).then(() => {
-      expect(fs.existsSync(path.join(testPath, 'sub', 'my-test-component', 'my-test-component.component.ts'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'sub', 'my-test-component', 'my-test-component.component.spec.ts'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'sub', 'my-test-component', 'my-test-component.component.scss'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'sub', 'my-test-component', 'my-test-component.component.html'))).to.equal(true);
-      expect(fs.existsSync(path.join(testPath, 'sub', 'my-test-component', 'my-test-component.controller.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'sub', 'my-test-component', 'my-test-component.component.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'sub', 'my-test-component', 'my-test-component.component.spec.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'sub', 'my-test-component', 'my-test-component.component.scss'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'sub', 'my-test-component', 'my-test-component.component.html'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'sub', 'my-test-component', 'my-test-component.controller.ts'))).to.equal(true);
+
+      done();
+    }).catch((err) => done(err));
+  });
+
+  it ('should generate component ../my-test-component', (done) => {
+    fs.mkdirSync(path.join(appDir, 'sub'));
+    process.chdir(path.join(appDir, 'sub'));
+
+    Generate.create(BLUEPRINT, '../my-test-component', {
+      quiet: true
+    }).then(() => {
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.spec.ts'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.scss'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.component.html'))).to.equal(true);
+      expect(fs.existsSync(path.join(project.root, appDir, 'my-test-component', 'my-test-component.controller.ts'))).to.equal(true);
 
       done();
     }).catch((err) => done(err));
