af39ed641f58b179fee67bf5c17f774d6e0b6f2a
Valentin GOT <gotvalentin@gmail.com>
2017-08-02 14:46:18 +0200

    fix(app): angular-ui-router has been renamed to @uirouter/angularjs
    Closes #20

--- templates/application/src/_vendor.ts
+++ templates/application/src/_vendor.ts
@@ -8,7 +8,7 @@ import 'angular-cookies';<% if (ngMaterial) { %>
 import 'angular-material';<% } else if (bootstrap) { %>
 import 'angular-touch';
 import 'angular-ui-bootstrap';<% } %>
-import 'angular-ui-router';
+import '@uirouter/angularjs';
 import 'restangular';
 import 'oclazyload';
 
